ecf9ccbda1dd194c347f3091e1e44d70a383ddd2
liam-leny <liamleny@protonmail.com>
2024-11-29 11:20:20 +0100

    Init

---
+++ src/controllers/lists.controller.ts
@@ -0,0 +1,27 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { ITodoList } from "../interfaces";
+
+export async function listLists(request: FastifyRequest, reply: FastifyReply) {
+  console.log("DB status", this.level.listsdb.status);
+  const listsIter = this.level.listsdb.iterator();
+
+  const result: ITodoList[] = [];
+  for await (const [key, value] of listsIter) {
+    result.push(JSON.parse(value));
+  }
+  reply.send(result);
+}
+
+export async function addList(request: FastifyRequest, reply: FastifyReply) {
+  const list = request.body as ITodoList;
+
+  if (!list || !list.id || !list.description) {
+    return reply.badRequest("Invalid list data.");
+  }
+
+  const result = await this.level.listsdb.put(
+    list.id.toString(),
+    JSON.stringify(list)
+  );
+  reply.code(201).send({ message: "List added successfully", data: list });
+}
---
+++ src/routes/lists/index.ts
@@ -0,0 +1,11 @@
+import { FastifyInstance } from "fastify";
+import * as listsController from "../../controllers/lists.controller";
+
+async function lists(fastify: FastifyInstance) {
+  fastify.get("/", listsController.listLists);
+
+  // TODO implement addList in controller
+  fastify.post("/", listsController.addList);
+}
+
+export default lists;
