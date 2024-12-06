16d136dc2f9737a315c818dc9ca1583b5bfa24c4
liam-leny <liamleny@protonmail.com>
2024-12-06 11:08:44 +0100

    Fix package and add docker

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,26 +1,41 @@
 // import { ITodoList, TodoListStatus } from './api-types'
-import { ListsApi } from 'todo-list-client'
-import axios from 'axios'
+import { ListsApi } from "todo-list-client";
+import axios from "axios";
 
 const lists = [
-    {
-        id: '1',
-        name: 'Work Tasks'
-    }, 
-    {
-        'id': '2',
-        name: 'Personal Tasks'
-    }, 
-    {
-        'id': '3',
-        name: 'Shopping List'
-    }
-]
+  {
+    id: "1",
+    name: "Work Tasks",
+  },
+  {
+    id: "2",
+    name: "Personal Tasks",
+  },
+  {
+    id: "3",
+    name: "Shopping List",
+  },
+];
 const listItems: Record<string, string[]> = {
-    'Work Tasks': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter'],
-    'Personal Tasks': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter'],
-    'Shopping List': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter']
-}
+  "Work Tasks": [
+    "Buy groceries",
+    "Complete React project",
+    "Exercise for 30 minutes",
+    "Read a book chapter",
+  ],
+  "Personal Tasks": [
+    "Buy groceries",
+    "Complete React project",
+    "Exercise for 30 minutes",
+    "Read a book chapter",
+  ],
+  "Shopping List": [
+    "Buy groceries",
+    "Complete React project",
+    "Exercise for 30 minutes",
+    "Read a book chapter",
+  ],
+};
 
 // const api = new ListsApi(
 //     {
@@ -31,25 +46,25 @@ const listItems: Record<string, string[]> = {
 // )
 
 export const apiClient = {
-    getLists: async () => {
-        return Promise.resolve(lists)
-        // const response = await api.listsGet()
-        // return response.data
-    },
-    addList: async (listName: string) => {
-        lists.push({id: listName, name: listName})
-        console.debug('-- addList', listName, lists);
-        return Promise.resolve(lists)
-    },
-    getTodos: async (listName: string): Promise<string[]> => {
-        return Promise.resolve(listItems[listName])
-    },
-    addTodo: async (listName: string, todo: string) => {
-        console.debug('-- addTodo', listName, todo, listItems);
-        if (!listItems[listName]) {
-            listItems[listName] = []
-        }
-        listItems[listName].push(todo)
-        return Promise.resolve(listItems[listName])
-    },
-}
+  getLists: async () => {
+    return Promise.resolve(lists);
+    // const response = await api.listsGet()
+    // return response.data
+  },
+  addList: async (listName: string) => {
+    lists.push({ id: listName, name: listName });
+    console.debug("-- addList", listName, lists);
+    return Promise.resolve(lists);
+  },
+  getTodos: async (listName: string): Promise<string[]> => {
+    return Promise.resolve(listItems[listName]);
+  },
+  addTodo: async (listName: string, todo: string) => {
+    console.debug("-- addTodo", listName, todo, listItems);
+    if (!listItems[listName]) {
+      listItems[listName] = [];
+    }
+    listItems[listName].push(todo);
+    return Promise.resolve(listItems[listName]);
+  },
+};
